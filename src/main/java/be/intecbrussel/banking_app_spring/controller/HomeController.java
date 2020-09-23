f9a268183733af2b95923504e76b75530adafdf4
Khuramalicontact <56122783+Khuramalicontact@users.noreply.github.com>
2020-09-23 12:20:41 +0200

    new

--- src/main/java/be/intecbrussel/banking_app_spring/controller/HomeController.java
+++ src/main/java/be/intecbrussel/banking_app_spring/controller/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+
     @GetMapping("/")
     public String homePage(Model model){
 //        model.addAttribute("client",new Client());
